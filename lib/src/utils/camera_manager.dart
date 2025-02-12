6229c66732b584f96ca85aa5452da2583271b4d0
crapo16 <crapo16@users.noreply.github.com>
2025-02-11 22:53:37 -0300

    Update camera_manager.dart

--- lib/src/utils/camera_manager.dart
+++ lib/src/utils/camera_manager.dart
@@ -1,42 +1,42 @@
-
-// This class manages the available cameras (back, front, and UVC) and handles the camera selection and video track retrieval.
 import 'package:flutter_webrtc/flutter_webrtc.dart';
 
 class CameraManager {
-  // A map to hold the names of the available cameras
-  static const _cameraNames = {
-    'back': 'Back camera',
-    'front': 'Front camera',
-    'uvc': 'UVC camera'
-  };
-
-  // A map to hold the available camera capturers
-  final _cameras = <String, WebRTCVideoCapturer>{};
+  final Map<String, MediaDeviceInfo> _cameras = {};
   String _selectedCamera = 'back';
 
   CameraManager() {
     _init();
   }
 
-  // Initialization method to get the list of available cameras and store them in the _cameras map
-  void _init() async {
-    final availableCameras = await WebRTC.getCameras();
-    for (final camera in availableCameras) {
-      _cameras[camera.label] = camera;
+  Future<void> _init() async {
+    List<MediaDeviceInfo> devices = await navigator.mediaDevices.enumerateDevices();
+    for (var device in devices.where((d) => d.kind == 'videoinput')) {
+      final label = device.label.toLowerCase();
+      if (label.contains('front')) {
+        _cameras['front'] = device;
+      } else if (label.contains('back')) {
+        _cameras['back'] = device;
+      } else {
+        _cameras['uvc'] = device;
+      }
     }
   }
 
-  // Method to select a camera
   void selectCamera(String camera) {
     _selectedCamera = camera;
   }
 
-  // Method to get the video track of the selected UVC camera
-  Future<WebRTCVideoTrack> getUVCVideoTrack() async {
-    if (_selectedCamera != 'uvc') {
-      throw Exception("UVC camera is not selected");
-    }
-    final capturer = _cameras[_selectedCamera];
-    return capturer.getTrack();
+  Future<MediaStreamTrack> getUVCVideoTrack() async {
+    if (_selectedCamera != 'uvc') throw Exception("UVC camera is not selected");
+    final device = _cameras[_selectedCamera];
+    if (device == null) throw Exception("No UVC camera found");
+    final constraints = {
+      'audio': false,
+      'video': {
+        'deviceId': device.deviceId,
+      },
+    };
+    final stream = await navigator.mediaDevices.getUserMedia(constraints);
+    return stream.getVideoTracks().first;
   }
 }
